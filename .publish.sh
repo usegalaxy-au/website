4e448757376c50650a65995d3712e758a4962aa6
Slugger70 <slugger70@gmail.com>
2018-12-05 13:04:02 +1100

    Modified publish for galaxy aus.

--- .publish.sh
+++ .publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
-
+DATE=$(date);
 TMPREPO=$(mktemp -d);
 git clone --quiet git@github.com:usegalaxy-eu/usegalaxy-eu.github.io.git $TMPREPO;
 mv $TMPREPO/.git _site/;
@@ -9,11 +9,9 @@ cp readme-deployed.md _site/README.md;
 rm -rf $TMPREPO
 cd _site
 git add --all *
-git config --local user.name "UseGalaxy.EU Build Bot"
-git config --local user.email "jenkins@usegalaxy.eu"
+#git config --local user.name "UseGalaxy.EU Build Bot"
+#git config --local user.email "jenkins@usegalaxy.eu"
 
-git commit -m "Update site ($BUILD_NUMBER)
-
-$BUILD_URL"
+git commit -m "Update site ($DATE)"
 
 git push origin master
